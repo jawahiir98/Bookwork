dfdd361e80890f14bb927d209ec92fb0e4ee32cc
Jawahiir <jawahiirboss@gmail.com>
2024-03-22 21:13:12 +0600

    Add project files.

---
+++ BookworkWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookworkWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
