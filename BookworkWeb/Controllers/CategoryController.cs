16cacc3880898968a813c8a081c86f3c1bbbe23a
Jawahiir <jawahiirboss@gmail.com>
2024-03-28 00:33:47 +0600

    Create Category table and display category list to View

---
+++ BookworkWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BookworkWeb.Data;
+using BookworkWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookworkWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        public CategoryController(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categoryList = db.Categories.ToList();   
+            return View(categoryList);
+        }
+    }
+}
---
+++ BookworkWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using BookworkWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookworkWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+                
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Test",
+                    DisplayOrder = 1
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Action",
+                    DisplayOrder = 2
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Sci-fi",
+                    DisplayOrder = 3
+                }
+            ) ;
+        }
+    }
+}
