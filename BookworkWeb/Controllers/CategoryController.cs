8cbbad7686a0abe55913ded96fc6f8201b2bde51
Jawahiir <jawahiirboss@gmail.com>
2024-03-28 21:08:24 +0600

    Edit, Delete and Toastr [Category]

--- BookworkWeb/Controllers/CategoryController.cs
+++ BookworkWeb/Controllers/CategoryController.cs
@@ -31,8 +31,51 @@ namespace BookworkWeb.Controllers
             {
                 db.Categories.Add(category);
                 db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             } return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? cat = db.Categories.FirstOrDefault(u => u.Id == id);
+            if (cat == null) return NotFound();
+            return View(cat);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Update(category);
+                db.SaveChanges();
+                TempData["success"] = "Category edited successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? cat = db.Categories.FirstOrDefault(u => u.Id == id);
+            if (cat == null) return NotFound();
+            return View(cat);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? cat = db.Categories.FirstOrDefault(c => c.Id == id);
+            if(cat == null) return NotFound();
+            db.Categories.Remove(cat);
+            db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
         }
     }
 }
